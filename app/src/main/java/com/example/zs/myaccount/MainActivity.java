0624872fec79812eae79f26a1493ce853b087375
Administrator <1635916212>
2016-09-02 20:45:00 +0800

    test

--- app/src/main/java/com/example/zs/myaccount/MainActivity.java
+++ app/src/main/java/com/example/zs/myaccount/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(tag,"wennm");
+        Log.i(tag,"wennm");
 
     }
     public void test(){
