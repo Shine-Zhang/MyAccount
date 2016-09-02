0c78147d209b72990d614f67fe3d8a3089f8b29b
zhangxudong <Zhang_xudong0011@163.com>
2016-09-03 01:04:03 +0800

    张旭东，新建了ReportFormPager类。在合并代码的时候，把基类BasePager合到原来的版本了

--- app/src/main/java/com/example/zs/pager/BasePager.java
+++ app/src/main/java/com/example/zs/pager/BasePager.java
@@ -12,7 +12,7 @@ import android.view.View;
  * 该类对应的是是Xml中所定义的布局文件ViewPager，该类所实现的是其业务逻辑
  * 同时，该类是新闻客户端主页面两大模块(从视觉上分),的整个上半部分的一个抽取
  */
-public abstract class BasePager {
+public class BasePager {
 
     //该Activity主要用来当作全局的山下文使用
     protected Activity mActivity;
@@ -31,15 +31,23 @@ public abstract class BasePager {
 
     /**
      * 该函数实现的是初始化布局，通过填充Xml文件，来返回相应的View
-     * 并初始化view中的相应控件
      */
-     public abstract View initView();
+     private View initView(){
+         //初始化布局,利用子类
+        // View view = mActivity.getLayoutInflater().inflate(R.layout.base_pager,null);
 
+	    //初始化布局中的控件
+
+         //这里暂时return null 否则会报错，大家正式开始写的时候返回的就应该是各自页面的布局文件所对应的那个view
+        return null;
+     }
 
     /**
      * 该函数实现的是初始化数据
      */
-    public abstract void initData();
+    public void initData(){
+
+    }
 
 
 }
