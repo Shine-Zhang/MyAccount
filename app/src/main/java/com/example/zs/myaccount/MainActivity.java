595809715dcb0890bad32547adc1bed48efb9476
wuautumn_wuqi <719031583@qq.com>
2016-09-02 22:56:22 +0800

    test7

--- app/src/main/java/com/example/zs/myaccount/MainActivity.java
+++ app/src/main/java/com/example/zs/myaccount/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(tag,"wennm");
-        Log.i(tag,"wennm");
     }
     public void test(){
 
