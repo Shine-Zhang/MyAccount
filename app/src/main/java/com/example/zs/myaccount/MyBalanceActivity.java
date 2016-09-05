a0c8e7550ccbfa3224778f2fc04cec026ade2f10
大油桃 <455430017@qq.com>
2016-09-05 09:44:43 +0800

    From:辛昊 更新图标 mipmip里的图标大家找不到

--- app/src/main/java/com/example/zs/myaccount/MyBalanceActivity.java
+++ app/src/main/java/com/example/zs/myaccount/MyBalanceActivity.java
@@ -8,7 +8,7 @@ public class MyBalanceActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_balance);
+        //setContentView(R.layout.activity_my_balance);
 
 
     }
