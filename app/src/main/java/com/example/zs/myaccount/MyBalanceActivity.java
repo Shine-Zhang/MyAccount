f73691dccbd4f64c44850c2b22cfefd2bdddbce7
zhongyunting <zhongyuntingting@163.com>
2016-09-05 10:55:16 +0800

    提交人：钟云婷      本次提交，解决了在Manifest中找不到MyBalance.Activity的问题

--- app/src/main/java/com/example/zs/myaccount/MyBalanceActivity.java
+++ app/src/main/java/com/example/zs/myaccount/MyBalanceActivity.java
@@ -9,7 +9,5 @@ public class MyBalanceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_balance);
-
-
     }
 }
