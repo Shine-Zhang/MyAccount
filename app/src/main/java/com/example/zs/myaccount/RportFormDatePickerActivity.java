db6de89979a0bef34b3cd8ec007c1deae31a4754
zhangxudong <Zhang_xudong0011@163.com>
2016-09-06 22:51:03 +0800

    提交人：张旭东 提交内容：增加了用来调整日期的ReportFormFatePickerActivity

---
+++ app/src/main/java/com/example/zs/myaccount/RportFormDatePickerActivity.java
@@ -0,0 +1,13 @@
+package com.example.zs.myaccount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RportFormDatePickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rport_form_date_picker);
+    }
+}
