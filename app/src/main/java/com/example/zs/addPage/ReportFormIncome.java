f5e262fefc4fad4f9c143d143a0b1d841bc9e97d
zhangxudong <Zhang_xudong0011@163.com>
2016-09-08 10:44:16 +0800

    提交人：张旭东 提交内容：增加了报表页面里的收入类

---
+++ app/src/main/java/com/example/zs/addPage/ReportFormIncome.java
@@ -0,0 +1,23 @@
+package com.example.zs.addPage;
+
+import android.app.Activity;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/9/8 0008.
+ */
+public class ReportFormIncome extends AddBasePage{
+
+    public ReportFormIncome(Activity activity) {
+        super(activity);
+        initData();
+    }
+
+    @Override
+    public View initView() {
+        return null;
+    }
+
+    private void initData() {
+    }
+}
