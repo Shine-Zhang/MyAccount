348a44974ec48eec18a110e4cf14689fcfb93218
zs <1132045742@qq.com>
2016-11-23 12:09:40 +0800

    提交人：张帅 重新整理了版本控制,哈,哈哈哈

---
+++ app/src/main/java/com/example/zs/listener/OnArriveTopListener.java
@@ -0,0 +1,12 @@
+package com.example.zs.listener;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2016/11/15 0015.
+ */
+public interface OnArriveTopListener {
+
+    void onArriveTop(Integer rid,TextView catagory,String accountString,int color);
+}
